b7631b9e07f72c08eed35b11c9a53a364bfb6bcc
Justin Dugger <jldugger@gmail.com>
2017-05-11 15:11:39 -0700

    add a line to summarize data

--- bin/hours.sh
+++ bin/hours.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 csvtool col 13,23 "$1" | sort | awk -F'",' '{a[$1]+=$2;}END{for(i in a)print i,"\",",a[i];}' | sort
+
+echo -n "total: "
+csvtool col 13,23 "$1" | sort | awk -F'",' '{a[$1]+=$2;}END{for(i in a)print i,"\",",a[i];}' | sort | cut -f 3 -d ',' | tr -d ' ' | numsum
